b07a593e77fb1e16d758f91cb97e705ea8714e4e
James Yang <james.yang92@gmail.com>
2015-01-16 18:12:08 -0500

    added regex parsers to grab boundary and block string data

--- hw1/studentcode/load_map.m
+++ hw1/studentcode/load_map.m
@@ -3,5 +3,10 @@ function map = load_map(filename, xy_res, z_res, margin)
 %  MAP = LOAD_MAP(filename, xy_res, z_res, margin).  Creates an occupancy grid
 %  map where a node is considered fill if it lies within 'margin' distance of
 %  on abstacle.
+text = fileread(filename);
+C = strsplit(text,'\n')
+boundary = regexp(C,'^\s*boundary(\s*\d+\.?\d*){6}')
+blocks = regexp(C,'^\s*block(\s*\d+\.?\d*){9}')
+
 map = zeros(0, 0);
 end

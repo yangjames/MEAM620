b5d423c569f7675a8d3c97a24e835b4f64926fc3
James Yang <james.yang92@gmail.com>
2015-01-16 12:12:11 -0500

    Revert "added hw1 code"
    This reverts commit 4cabc9f7aca77e048fd0fa0073798fe10b1c3232.

--- hw1/studentcode/collide.m
+++
@@ -1,7 +0,0 @@
-function [C] = collide(map, points)
-% COLLIDE Test whether points collide with an obstacle in an environment.
-%   C = collide(map, points).  points is an M-by-3 matrix where each
-%   row is an (x, y, z) point.  C in an M-by-1 logical vector;
-%   C(i) = 1 if M(i, :) touches an obstacle and is 0 otherwise.
-C = [];
-end

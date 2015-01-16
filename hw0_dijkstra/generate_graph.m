6bd069ce46f3452c18ab02983a1ade3e37645fee
James Yang <james.yang92@gmail.com>
2015-01-16 12:11:01 -0500

    Revert "decent example graph and graph generator"
    This reverts commit fdbb7f1c255e62e566aa18f7aaa4d9bd0750bf5b.

--- hw0_dijkstra/generate_graph.m
+++
@@ -1,9 +0,0 @@
-function G = generate_graph(num_nodes, num_edges)
-G = zeros(num_edges,3);
-G(:,1:2) = ceil(rand(num_edges,2)*num_nodes);
-G(:,3) = rand(num_edges,1);
-indicator = G(:,1) == G(:,2);
-while sum(indicator) > 0
-    G(indicator,1:2) = ceil(rand(sum(indicator),2)*num_nodes);
-    indicator = G(:,1) == G(:,2);
-end

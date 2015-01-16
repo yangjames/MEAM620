4cabc9f7aca77e048fd0fa0073798fe10b1c3232
James Yang <james.yang92@gmail.com>
2015-01-16 12:10:25 -0500

    added hw1 code

---
+++ hw1/studentcode/load_map.m
@@ -0,0 +1,7 @@
+function map = load_map(filename, xy_res, z_res, margin)
+% LOAD_MAP Load a map from disk.
+%  MAP = LOAD_MAP(filename, xy_res, z_res, margin).  Creates an occupancy grid
+%  map where a node is considered fill if it lies within 'margin' distance of
+%  on abstacle.
+map = zeros(0, 0);
+end

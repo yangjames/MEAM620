a8ac6b165bf19433160c5651f01d17dd3824b4f2
James Yang <james.yang92@gmail.com>
2015-01-16 18:49:58 -0500

    can parse all boundary and obstacle data. need to make map now

--- hw1/studentcode/load_map.m
+++ hw1/studentcode/load_map.m
@@ -3,10 +3,30 @@ function map = load_map(filename, xy_res, z_res, margin)
 %  MAP = LOAD_MAP(filename, xy_res, z_res, margin).  Creates an occupancy grid
 %  map where a node is considered fill if it lies within 'margin' distance of
 %  on abstacle.
+
+%% parse text file for boundary and obstacles
+% read in text file
 text = fileread(filename);
-C = strsplit(text,'\n')
-boundary = regexp(C,'^\s*boundary(\s*\d+\.?\d*){6}')
-blocks = regexp(C,'^\s*block(\s*\d+\.?\d*){9}')
+
+% split text by line
+split_texts = strsplit(text,'\n');
+
+% find uncommented and otherwise valid phrases
+valid_boundary = regexp(split_texts,'^\s*boundary(\s*\d+\.?\d*){6}');
+valid_blocks = regexp(split_texts,'^\s*block(\s*\d+\.?\d*){9}');
+
+% get indices of valid phrases
+blocks_idx = find(~cellfun(@isempty, valid_blocks))
+
+% extract boundary and obstacle values
+boundary_text = split_texts{~cellfun(@isempty,valid_boundary)};
+boundary = cell2mat(textscan(boundary_text(length('boundary')+1:end), '%f'));
+blocks = zeros(9,length(blocks_idx));
+for i = 1:length(blocks_idx)
+    block_text = split_texts{blocks_idx(i)};
+    blocks(:,i) = cell2mat(textscan(block_text(length('block')+1:end), '%f'));
+end
+
 
 map = zeros(0, 0);
 end

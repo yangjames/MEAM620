6375d6d1ace3e8d28fbefc45dd1d1b06268e43b2
James Yang <james.yang92@gmail.com>
2015-01-18 16:45:44 -0500

    added code for plotting path. currently draws environment bounds plus obstacles

--- hw1/studentcode/load_map.m
+++ hw1/studentcode/load_map.m
@@ -4,6 +4,7 @@ function map = load_map(filename, xy_res, z_res, margin)
 %  map where a node is considered fill if it lies within 'margin' distance of
 %  on abstacle.
 
+%{
 %% parse text file for boundary and obstacles
 % read in text file
 text = fileread(filename);
@@ -20,13 +21,39 @@ blocks_idx = find(~cellfun(@isempty, valid_blocks));
 
 % extract boundary and obstacle values
 boundary_text = split_texts{~cellfun(@isempty,valid_boundary)};
-boundary = cell2mat(textscan(boundary_text(length('boundary')+1:end), '%f'))
+boundary = cell2mat(textscan(boundary_text(length('boundary')+1:end), '%f'));
 blocks = zeros(9,length(blocks_idx));
 for i = 1:length(blocks_idx)
     block_text = split_texts{blocks_idx(i)};
     blocks(:,i) = cell2mat(textscan(block_text(length('block')+1:end), '%f'));
 end
-blocks
+%}
+
+%% alternate version
+fid = fopen(filename);
+C = textscan(fid,'%s %f %f %f %f %f %f %f %f %f',...
+    'TreatAsEmpty',{'NA','na'},'CommentStyle','#');
+fclose(fid);
+blocks = cell2mat(C(2:end));
+
+boundary_idx = find(strcmp('boundary',C{1}));
+boundary = blocks(boundary_idx,1:6);
+blocks(boundary_idx,:) = [];
+
 %% create map
-map = zeros(0, 0);
+num_rows = ceil(boundary(4)/xy_res);
+num_cols = ceil(boundary(5)/xy_res);
+num_plane = num_rows*num_cols;
+num_depth = ceil(boundary(6)/z_res);
+num_nodes = num_plane*num_depth;
+
+
+
+map.boundary = boundary;
+map.blocks = blocks;
+map.xy_res = xy_res;
+map.z_res = z_res;
+map.margin = margin;
+
+plot_path(map,1);
 end
--- hw1/studentcode/plot_path.m
+++ hw1/studentcode/plot_path.m
@@ -3,4 +3,21 @@ function plot_path(map, path)
 %   PLOT_PATH(map, path) creates a figure showing a path through the
 %   environment.  path is an N-by-3 matrix where each row corresponds to the
 %   (x, y, z) coordinates of one point along the path.
+figure(1)
+clf
+[x,y,z] = get_rect_vals(map.boundary);
+h(1) = patch(x,y,z,'w','FaceAlpha',0.0);
+hold on
+xlabel('x')
+ylabel('y')
+zlabel('z')
+axis equal
+xlim(map.boundary([1,4]))
+ylim(map.boundary([2,5]))
+zlim(map.boundary([3,6]))
+grid on
+for i = 2:size(map.blocks,1)
+    [x,y,z] = get_rect_vals(map.blocks(i,:));
+    h(i) = patch(x,y,z,map.blocks(i,7:9)/255);
+end
 end

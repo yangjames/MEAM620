fcd7e6c16403a36de09f6cf65d9df94fd7f8aff5
James Yang <james.yang92@gmail.com>
2015-01-18 18:13:23 -0500

    unverified collision check

--- hw1/studentcode/collide.m
+++ hw1/studentcode/collide.m
@@ -3,5 +3,9 @@ function [C] = collide(map, points)
 %   C = collide(map, points).  points is an M-by-3 matrix where each
 %   row is an (x, y, z) point.  C in an M-by-1 logical vector;
 %   C(i) = 1 if M(i, :) touches an obstacle and is 0 otherwise.
-C = [];
+
+%% check if points are inside the boundaries
+collisions = bsxfun(@plus,[map.boundary; map.blocks],...
+    [map.margin map.margin map.margin -map.margin -map.margin -map.margin]);
+C = sum([bsxfun(@geq, collisions(:,1:3), points) bsxfun(@leq, collisions(:,4:6), points)],2) > 0;
 end

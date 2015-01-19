a66ed6069fbf02624b7abd1e51e213696cde3a02
James Yang <james.yang92@gmail.com>
2015-01-18 19:14:53 -0500

    fixed more conversion stuff

--- hw1/studentcode/node_to_xyz.m
+++ hw1/studentcode/node_to_xyz.m
@@ -1,11 +1,11 @@
 function coordinate = node_to_xyz(map,node)
 % returns an nx3 matrix of xyz coordinates corresponding to node value
 
-num_rows = floor(map.boundary(4)/map.xy_res);
-num_cols = floor(map.boundary(5)/map.xy_res);
+num_rows = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
+num_cols = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
 num_plane = num_rows*num_cols;
 
-mod(node,num_plane)
-coordinate = [mod(mod(node,num_plane),num_cols)*map.xy_res ...
-                floor(mod(node,num_plane)/num_rows)*map.xy_res ...
-                round(node/num_plane)*map.z_res];
+
+coordinate = [(mod(mod(node-1,num_plane),num_cols))*map.xy_res + map.boundary(1) ...
+                floor(mod(node-1,num_plane)/num_rows)*map.xy_res + map.boundary(2) ...
+                floor((node-1)/num_plane)*map.z_res + map.boundary(3)];
--- hw1/studentcode/xyz_to_node.m
+++ hw1/studentcode/xyz_to_node.m
@@ -1,12 +1,12 @@
 function node = xyz_to_node(map,coordinate)
 % returns a column vector of nodes mapped to a coordinate
 
-num_rows = floor(map.boundary(4)/map.xy_res);
-num_cols = floor(map.boundary(5)/map.xy_res);
+num_rows = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
+num_cols = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
 num_plane = num_rows*num_cols;
 
-z_comp = floor(coordinate(:,3)/map.z_res)*num_plane;
-y_comp = floor(coordinate(:,2)/map.xy_res)*num_rows;
-x_comp = floor(coordinate(:,1)/map.xy_res);
+z_comp = floor((coordinate(:,3)-map.boundary(3))/map.z_res)*num_plane;
+y_comp = floor((coordinate(:,2)-map.boundary(2))/map.xy_res)*num_rows;
+x_comp = floor((coordinate(:,1)-map.boundary(1))/map.xy_res);
 
 node = x_comp + y_comp + z_comp;

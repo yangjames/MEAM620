f94ab10eeb2b5196906711b2703a59507e388186
James Yang <james.yang92@gmail.com>
2015-01-18 21:25:05 -0500

    added path plotting

--- hw1/studentcode/plot_path.m
+++ hw1/studentcode/plot_path.m
@@ -20,4 +20,7 @@ for i = 2:size(map.blocks,1)
     [x,y,z] = get_rect_vals(map.blocks(i,:));
     h(i) = patch(x,y,z,map.blocks(i,7:9)/255);
 end
+if ~isempty(path)
+    h(end+1) = plot3(path(:,1),path(:,2),path(:,3),'k-');
+end
 end

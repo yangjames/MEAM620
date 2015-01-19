db879f257ccaea525a72e9d45bf5a85a32b2e36d
James Yang <james.yang92@gmail.com>
2015-01-19 12:23:34 -0500

    fixed a regex flag for negative numbers, but textscan seems to be slightly faster

--- hw1/studentcode/load_map.m
+++ hw1/studentcode/load_map.m
@@ -3,7 +3,6 @@ function map = load_map(filename, xy_res, z_res, margin)
 %  MAP = LOAD_MAP(filename, xy_res, z_res, margin).  Creates an occupancy grid
 %  map where a node is considered fill if it lies within 'margin' distance of
 %  on abstacle.
-
 %{
 %% parse text file for boundary and obstacles using regex
 % read in text file
@@ -13,20 +12,21 @@ text = fileread(filename);
 split_texts = strsplit(text,'\n');
 
 % find uncommented and otherwise valid phrases
-valid_boundary = regexp(split_texts,'^\s*boundary(\s*\d+\.?\d*){6}\s*$');
-valid_blocks = regexp(split_texts,'^\s*block(\s*\d+\.?\d*){9}\s*$');
+valid_boundary = regexp(split_texts,'^\s*boundary(\s*[-]?\d+\.?\d*){6}\s*$');
+valid_blocks = regexp(split_texts,'^\s*block(\s*[-]?\d+\.?\d*){9}\s*$');
 
 % get indices of valid phrases
 blocks_idx = find(~cellfun(@isempty, valid_blocks));
 
 % extract boundary and obstacle values
 boundary_text = split_texts{~cellfun(@isempty,valid_boundary)};
-boundary = cell2mat(textscan(boundary_text(length('boundary')+1:end), '%f'));
+boundary = cell2mat(textscan(boundary_text(length('boundary')+1:end), '%f'))';
 blocks = zeros(9,length(blocks_idx));
 for i = 1:length(blocks_idx)
     block_text = split_texts{blocks_idx(i)};
     blocks(:,i) = cell2mat(textscan(block_text(length('block')+1:end), '%f'));
 end
+blocks = blocks';
 %}
 
 %% parse text file for boundary and obstacles using textscan

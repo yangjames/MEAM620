e77f89bb2bb8297f590a37ea2838730605f4dd60
James Yang <james.yang92@gmail.com>
2015-01-19 17:10:09 -0500

    deleted a blank line

--- hw1/studentcode/node_to_xyz.m
+++ hw1/studentcode/node_to_xyz.m
@@ -5,7 +5,6 @@ num_x = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
 num_y = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
 num_plane = num_x*num_y;
 
-
 coordinate = [((mod(mod(node,num_plane),num_x))-1)*map.xy_res + map.boundary(1) ...
                 floor(mod(node,num_plane)/num_x)*map.xy_res + map.boundary(2) ...
                 floor(node/num_plane)*map.z_res + map.boundary(3)];

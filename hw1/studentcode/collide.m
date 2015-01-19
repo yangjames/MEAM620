50d05d0db491bb81692d0fc8ae510649d7ebc340
James Yang <james.yang92@gmail.com>
2015-01-19 15:09:53 -0500

    vectorized collision detection

--- hw1/studentcode/collide.m
+++ hw1/studentcode/collide.m
@@ -5,24 +5,23 @@ function [C] = collide(map, points)
 %   C(i) = 1 if M(i, :) touches an obstacle and is 0 otherwise.
 
 %% check for empty map
-if isempty(map.boundary)
+if isempty(map.boundary) || isempty(points)
     C = 0;
     return;
 end
 
 %% get true bounds
 margin = [map.margin map.margin map.margin -map.margin -map.margin -map.margin];
-%n_bound = map.boundary + margin;
-n_blocks = bsxfun(@minus, map.blocks(:,1:6), margin);
+n_bound = map.boundary;% + margin;
+n_blocks = repmat(bsxfun(@minus, map.blocks(:,1:6), margin),size(points,1),1);
 
+points = repmat(points,1,2);
 %% get points that are out of bounds
-%ob = sum(bsxfun(@gt, n_bound(1:3), points) | bsxfun(@lt, n_bound(4:6), points),2)>0;
+ob = sum(bsxfun(@gt, n_bound(1:3), points) | bsxfun(@lt, n_bound(4:6), points),2)>0;
 
 %% get poitns that collide with an object
-co = false(size(points,1),1);
-for i = 1:size(points,1)
-    inside = sum(bsxfun(@le, n_blocks(:,1:3), points(i,:)) & bsxfun(@ge, n_blocks(:,4:6),points(i,:)),2) == 3;
-    co(i) = sum(inside)>0;
-end
-C = co;%|ob;
+idx = reshape(meshgrid(1:size(points,1),1:size(map.blocks,1)),size(map.blocks,1)*size(points,1),1);
+inside = sum(bsxfun(@le, n_blocks(:,1:3),points(idx,:)) & bsxfun(@ge, n_blocks(:,4:6),points(idx,:)),2) == 3;
+co = sum(reshape(inside',size(map.blocks,1),size(points,1)))'>0;
+C = co|ob;
 end

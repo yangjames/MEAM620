62855ab2f244b050c70f27ecb5ba389f590d541f
James Yang <james.yang92@gmail.com>
2015-01-19 00:27:14 -0500

    ACTUALLY fixed the conversion between coordinate number to node label...hopefully...

--- hw1/studentcode/node_to_xyz.m
+++ hw1/studentcode/node_to_xyz.m
@@ -1,11 +1,11 @@
 function coordinate = node_to_xyz(map,node)
 % returns an nx3 matrix of xyz coordinates corresponding to node value
 
-num_rows = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
-num_cols = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
-num_plane = num_rows*num_cols;
+num_x = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
+num_y = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
+num_plane = num_x*num_y;
 
 
-coordinate = [(mod(mod(node-1,num_plane),num_cols))*map.xy_res + map.boundary(1) ...
-                floor(mod(node-1,num_plane)/num_rows)*map.xy_res + map.boundary(2) ...
-                floor((node-1)/num_plane)*map.z_res + map.boundary(3)];
+coordinate = [((mod(mod(node,num_plane),num_x))-1)*map.xy_res + map.boundary(1) ...
+                floor(mod(node,num_plane)/num_x)*map.xy_res + map.boundary(2) ...
+                floor(node/num_plane)*map.z_res + map.boundary(3)];
--- hw1/studentcode/xyz_to_node.m
+++ hw1/studentcode/xyz_to_node.m
@@ -1,12 +1,12 @@
 function node = xyz_to_node(map,coordinate)
 % returns a column vector of nodes mapped to a coordinate
 
-num_rows = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
-num_cols = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
-num_plane = num_rows*num_cols;
+num_x = floor((map.boundary(4)-map.boundary(1))/map.xy_res)+1;
+num_y = floor((map.boundary(5)-map.boundary(2))/map.xy_res)+1;
+num_plane = num_x*num_y;
 
-z_comp = floor((coordinate(:,3)-map.boundary(3))/map.z_res)*num_plane;
-y_comp = floor((coordinate(:,2)-map.boundary(2))/map.xy_res)*num_rows;
-x_comp = floor((coordinate(:,1)-map.boundary(1))/map.xy_res);
+z_comp = round((coordinate(:,3)-map.boundary(3))/map.z_res)*num_plane;
+y_comp = round((coordinate(:,2)-map.boundary(2))/map.xy_res)*num_x;
+x_comp = round((coordinate(:,1)-map.boundary(1))/map.xy_res)+1;
 
 node = x_comp + y_comp + z_comp;

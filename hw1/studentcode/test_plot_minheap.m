9809934369152e9b3cdfe42d6ff37b0a278ab644
James Yang <james.yang92@gmail.com>
2015-01-20 01:12:02 -0500

    binary heap visualizer

---
+++ hw1/studentcode/test_plot_minheap.m
@@ -0,0 +1,17 @@
+function test_plot_minheap(Q,len)
+
+num_nodes = len;
+
+figure(9)
+clf
+hold on
+
+for ii = 1:num_nodes
+    y = floor(log2(ii));
+    x = (ii-2^y)/2^(y-1) - 1 + 2^(-y);
+    text(x,y,num2str(Q(ii)))
+end
+    
+hold off
+
+set(gca,'xlim',[-1,1],'ylim',[-1,floor(log2(ii))+1],'ydir','reverse')

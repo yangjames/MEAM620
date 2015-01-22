325e3f6db261cd56f2dd7d0eea6642b52213d3d6
James Yang <james.yang92@gmail.com>
2015-01-21 20:30:26 -0500

    just tried different versions of dijkstra

--- hw1/studentcode/dijkstra_test.m
+++ hw1/studentcode/dijkstra_test.m
@@ -16,19 +16,21 @@ verifyEqual(testcase, path(end, :), stop);
 end
 
 function testMap1(testcase)
-map = load_map('sample_maps/map0.txt', 0.1, 2.0, 0.3);
+map = load_map('sample_maps/map0.txt', 0.1, 2.0, 0.3)
 start = [0.0  -4.9 0.2];
 stop  = [8.0  18.0 3.0];
-[path, nodes_expanded] = dijkstra(map, start, stop, true)
-%plot_path(map,path,3,4);
-save('path.mat','path','nodes_expanded');
+[path, nodes_expanded] = dijkstrav3(map, start, stop, true)
+%plot_path(map,path);
 assertPathValid(testcase, map, path, start, stop, [0 10], [-5 20], [0 6]);
 end
-
+%{
 function testEmptyMap(testcase)
-map = load_map('sample_maps/emptyMap.txt', 0.25, 10.0, 4.0);
+map = load_map('sample_maps/emptyMap.txt', 0.25, 10.0, 4.0)
 start = [1.81 2.40, 5.5];
-stop = [9.8 8.7 0.2];
-path = dijkstra(map, start, stop, true)
+stop = [10 10 0];
+%stop = [9.8 8.7 0.2];
+[path, nodes_expanded] = dijkstrav3(map, start, stop, true)
+%plot_path(map,path);
 assertPathValid(testcase, map, path, start, stop, [0 10], [0 10], [0 6]);
 end
+%}

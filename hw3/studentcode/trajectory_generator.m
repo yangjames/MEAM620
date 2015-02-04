a56c916b59763ff98effede7e3b67f0e86891908
James Yang <james.yang92@gmail.com>
2015-02-03 22:28:28 -0500

    added proj1 phase 3 code

---
+++ hw3/studentcode/trajectory_generator.m
@@ -0,0 +1,23 @@
+function [ desired_state ] = trajectory_generator(t, qn, map, path)
+% TRAJECTORY_GENERATOR: Turn a Dijkstra or A* path into a trajectory
+%
+% NOTE: This function would be called with variable number of input
+% arguments. In init_script, it will be called with arguments
+% trajectory_generator([], [], map, path) and later, in test_trajectory,
+% it will be called with only t and qn as arguments, so your code should
+% be able to handle that. This can be done by checking the number of
+% arguments to the function using the "nargin" variable, check the
+% MATLAB documentation for more information.
+%
+% map: The map structure returned by your load_map function
+% path: This is the path returned by your planner (dijkstra function)
+%
+% desired_state: Contains all the information that is passed to the
+% controller, as in phase 2
+%
+% It is suggested to use "persistent" variables to store map and path
+% during the initialization call of trajectory_generator, e.g.
+% persistent map0 path0
+% map0 = map;
+% path0 = path;
+

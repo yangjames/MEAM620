eb665969e0283fa6e004aa7d2a5562274c7bbbd9
James Yang <james.yang92@gmail.com>
2015-02-16 19:12:43 -0500

    fixed a bug in minimum n trajectory stuff and submission for hw 4

--- hw4/min_n_trajectory.m
+++ hw4/min_n_trajectory.m
@@ -22,22 +22,24 @@ end
 deg = 1:2*n;
 for i = 1:n-1
     coeff = deg-i-1;
-    coeff(coeff<=0)=0;
-    A(m*2+1+2*(i-1),1:2*n) = (factorial(deg-1)./factorial(coeff)).*(time_stamps(1).^((1:2*n)-i-1));
-    A(m*2+2+2*(i-1),end-2*n+1:end) = (factorial(deg-1)./factorial(coeff)).*(time_stamps(end).^((1:2*n)-i-1));
+    mask = coeff >= 0;
+    coeff(coeff < 0)=0;
+    A(m*2+1+2*(i-1),1:2*n) = (factorial(deg-1)./factorial(coeff)).*(time_stamps(1).^((1:2*n)-i-1)).*mask;
+    A(m*2+2+2*(i-1),end-2*n+1:end) = (factorial(deg-1)./factorial(coeff)).*(time_stamps(end).^((1:2*n)-i-1)).*mask;
     X(m*2+1+2*(i-1),:) = start_constraint(i,:);
     X(m*2+2+2*(i-1),:) = end_constraint(i,:);
 end
-A(isnan(A) | isinf(A)) = 0;
 
 % add in between constraints
 for i = 1:2*(n-1)
     coeff = deg-i-1;
-    coeff(coeff<=0)=0;
+    mask = coeff >= 0;
+    coeff(coeff < 0)=0;
     for j = 1:m-1
         A((i-1)*(m-1)+j+end-2*(n-1),(j-1)*2*n+1:(j-1)*2*n+4*n)=...
-            [(factorial(deg-1)./factorial(coeff)).*(time_stamps(j+1).^((1:2*n)-i-1)) -(factorial(deg-1)./factorial(coeff)).*(time_stamps(j+1).^((1:2*n)-i-1))];
+            [(factorial(deg-1)./factorial(coeff)).*(time_stamps(j+1).^((1:2*n)-i-1)) -(factorial(deg-1)./factorial(coeff)).*(time_stamps(j+1).^((1:2*n)-i-1))].*[mask mask];
     end
 end
-C=A;
-%C = A\X;
+
+A(isnan(A) | isinf(A)) = 0;
+C = A\X;

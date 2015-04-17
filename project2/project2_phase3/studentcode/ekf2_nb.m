cc617f5d042fbc1973e0e1663d73a3122a6b1f42
James Yang <james.yang92@gmail.com>
2015-04-17 16:35:00 -0400

    fixed shit

---
+++ project2/project2_phase3/studentcode/ekf2_nb.m
@@ -0,0 +1,116 @@
+function [X,Z] = ekf2_nb(sensor, params)
+% EKF2 Extended Kalman Filter with IMU as inputs
+%
+% INPUTS:
+%   sensor - struct stored in provided dataset, fields include
+%          - is_ready: logical, indicates whether sensor data is valid
+%          - t: sensor timestamp
+%          - rpy, omg, acc: imu readings
+%          - img: uint8, 240x376 grayscale image
+%          - id: 1xn ids of detected tags
+%          - p0, p1, p2, p3, p4: 2xn pixel position of center and
+%                                four corners of detected tags
+%            Y
+%            ^ P3 == P2
+%            | || P0 ||
+%            | P4 == P1
+%            o---------> X
+%   varargin - any variables you wish to pass into the function, could be
+%              a data structure to represent the map or camera parameters,
+%              your decision. But for the purpose of testing, since we don't
+%              know what inputs you will use, you have to specify them in
+%              init_script by doing
+%              ekf1_handle = ...
+%                  @(sensor) ekf2(sensor, your input arguments);
+%
+% OUTPUTS:
+% X - nx1 state of the quadrotor, n should be greater or equal to 9
+%     the state should be in the following order
+%     [x; y; z; vx; vy; vz; roll; pitch; yaw; other states you use]
+%     we will only take the first 9 rows of X
+% OPTIONAL OUTPUTS:
+% Z - mx1 measurement of your pose estimator, m shoulb be greater or equal to 6
+%     the measurement should be in the following order
+%     [x; y; z; roll; pitch; yaw; other measurement you use]
+%     note that this output is optional, it's here in case you want to log your
+%     measurement 
+persistent X_prev t_prev_sen P Q R
+
+if isempty(X_prev)
+    X_prev = zeros(9,1);
+    t_prev_sen = 0;
+    P = eye(9);
+    Q = eye(6);%bsxfun(@times,eye(12),[ones(1,3)*10 ones(1,3) ones(1,3) ones(1,3)]);
+    R = eye(9);
+    %Q = diag([ones(1,3)*0.5 ones(1,3)*0.1 ones(1,3)*1 ones(1,3)*2]);
+    %R = diag([ones(1,6)*0.0001 ones(1,3)*0.001]);
+end
+
+if ~sensor.is_ready
+    X = X_prev;
+    X = [X(1:3);X(7:9);X(4:6)];
+    Z = [];
+    return
+end
+X = X_prev;
+Z = [];
+   
+%% propagate
+dt = sensor.t-t_prev_sen;
+
+phi = X(4);
+theta = X(5);
+psi = X(6);
+phi_dot = sensor.omg(1);
+theta_dot = sensor.omg(2);
+psi_dot = sensor.omg(3);
+
+v_x = X_prev(7);
+v_y = X_prev(8);
+v_z = X_prev(9);
+a_x = sensor.acc(1);
+a_y = sensor.acc(2);
+a_z = sensor.acc(3);
+
+
+F = eye(9) + dt * params.A2_nb(phi,theta,psi,...
+                            a_x,a_y,a_z,...
+                            0,0,0,...
+                            0,0,...
+                            phi_dot,psi_dot);
+
+bt = params.f2_nb(phi,theta,psi,...
+    a_x,a_y,a_z,...
+    0,0,0,...
+    0,0,0,...
+    v_x,v_y,v_z,...
+    phi_dot,theta_dot,psi_dot)*dt;
+%}
+V = params.U2_nb(phi,theta,psi)*dt;
+
+X = X+bt;
+P = F*P*F'+ V*Q*V';
+
+%% update
+if ~isempty(sensor.id)
+    C = eye(9);
+
+    [p,q] = estimate_pose(sensor);
+    [p_dot,~] = estimate_vel(sensor);
+    Z = [p;q;p_dot];
+
+    K = P*C'/(C*P*C'+R);
+    X = X+K*(Z-C*X_prev);
+
+    P = (eye(9)-K*C)*P;
+    
+    Z = [Z(1:3);Z(7:9);Z(4:6)];
+end
+
+%% store new value
+X_prev = X;
+t_prev_sen = sensor.t;
+
+%% returned values need to be of certain format
+X = [X(1:3);X(7:9);X(4:6);X(10:end)];
+end
---
+++ project2/project2_phase3/studentcode/test_ekf2_nb.m
@@ -0,0 +1,74 @@
+%% load data
+clear all;
+close all;
+load data/studentdata4;
+
+init_script;
+ 
+%% interleave time vectors
+N = length(data);
+all_time = zeros(1,N);
+for i = 1:N
+    all_time(i) = data(i).t;
+end
+
+%% run filter
+X = [];
+Z = [];
+idx = 1;
+timing = [];
+tic
+for i=1:N
+    t_start = toc;
+    [X_out, Z_out] = ekf2_handle(data(i));
+    t_end = toc;
+    if ~isempty(X_out)
+        X(:,idx) = X_out;
+        if ~isempty(Z_out)
+            Z(:,idx) = Z_out;
+        else
+            Z(:,idx) = zeros(9,1);
+        end
+        timing(idx) = t_end-t_start;
+        idx = idx+1;
+    end
+end
+
+figure, grid on, hold on
+m_time = linspace(0,all_time(end),idx-1);
+subplot(3,3,1)
+plot(m_time,X(1,:),'r-',time,vicon(1,:),'b-',m_time,Z(1,:),'g--');
+title('x')
+subplot(3,3,2)
+plot(m_time,X(2,:),'r-',time,vicon(2,:),'b-',m_time,Z(2,:),'g--');
+title('y')
+subplot(3,3,3)
+plot(m_time,X(3,:),'r-',time,vicon(3,:),'b-',m_time,Z(3,:),'g--');
+title('z')
+
+subplot(3,3,4)
+plot(m_time,X(4,:),'r-',time,vicon(7,:),'b-',m_time,Z(4,:),'g--');
+title('$\dot{x}$','Interpreter','LaTeX')
+subplot(3,3,5)
+plot(m_time,X(5,:),'r-',time,vicon(8,:),'b-',m_time,Z(5,:),'g--');
+title('$\dot{y}$','Interpreter','LaTeX')
+subplot(3,3,6)
+plot(m_time,X(6,:),'r-',time,vicon(9,:),'b-',m_time,Z(6,:),'g--');
+title('$\dot{z}$','Interpreter','LaTeX')
+
+subplot(3,3,7)
+plot(m_time,X(7,:),'r-',time,vicon(4,:),'b-',m_time,Z(7,:),'g--');
+title('\phi')
+subplot(3,3,8)
+plot(m_time,X(8,:),'r-',time,vicon(5,:),'b-',m_time,Z(8,:),'g--');
+title('\theta')
+subplot(3,3,9)
+plot(m_time,X(9,:),'r-',time,vicon(6,:),'b-',m_time,Z(9,:),'g--');
+title('\psi')
+
+%{
+figure, grid on
+plot(1:idx-1,timing,'b--')
+ylabel('dt')
+xlabel('index')
+%}
